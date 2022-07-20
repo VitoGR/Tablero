e8764a2e22172b0c399454e691cc1dfb535cf9e8
Vito <106935881+VitoGR@users.noreply.github.com>
2022-07-20 19:08:24 -0300

    Update Tablero.java

--- src/tablero/Tablero.java
+++ src/tablero/Tablero.java
@@ -12,10 +12,43 @@ public class Tablero {
     public static final String ANSI_BLUE = "\u001B[34m";
 
     public static void main(String[] args) {
+        
+        /*
+         Un niño está jugando a colorear su tablero de ajedrez y va a pintar cada 
+        casilla toda de azul o toda de rojo. Para darle un toque personalizado, 
+        quiere pintar la misma cantidad de casillas rojas que de azules, pero no 
+        quiere que le queden dos columnas con la misma cantidad de casillas rojas 
+        pintadas, ni quiere que le queden dos filas con la misma cantidad de 
+        casillas rojas pintadas.
+        
+         ¿Puede lograr pintarlo cumpliendo esas condiciones?
+        
+         Respuesta: No es posible pintar el tablero siguiendo las condiciones, dado
+        que al menos tendria que repetirse una vez las mismas cantidades de casillas
+        rojas en las columna. Lo mismo sucede con con las filas.
+        
+         ¿Y si en vez de un tablero de ajedrez normal de 8x8 fuese un tablero 
+        de ajedrez gigante de 1000x1000?
+        
+         Respuesta: Como se demostró en el caso anterior, no es posible cumplir
+        con las condiciones, en una matriz cuadrada (8x8), (1000x1000), etc.
+        
+         Es importante incluir y detallar el razonamiento y/o programa usado 
+        para determinar la respuesta.
+        
+         Razonamiento: para entender el problema planteado observé el libro de 
+        colorear de un niño, después de un rato, concluí que el tablero debía 
+        estar pintado de manera diagonal, no tiene que seguir una lógica o tener
+        algún tipo de practicidad, como si lo hace un tablero que tiene sus 
+        casillas blancas y negras para poder guiarte a la hora de mover tus piezas 
+        sobre el mismo.
+ 
+        */
 
         Scanner sc = new Scanner(System.in);
-        System.out.println("Sr. usuario ingrese el tamaño de la matriz, recuerde que la misma sera cuadrada"
-                + "Por ej: si ingresa un (4) la matriz sera de diametro 4x4");
+        System.out.print("Sr. usuario ingrese el tamaño de la matriz, recuerde que la misma sera cuadrada"
+                + "\nPor ej: si ingresa un (4) la matriz sera de diametro 4x4: ");
+        
         int diametro = sc.nextInt();
         int contadorRojo = 0, contadorAzul = 0;
         String Azul = "A", Rojo = "R";
